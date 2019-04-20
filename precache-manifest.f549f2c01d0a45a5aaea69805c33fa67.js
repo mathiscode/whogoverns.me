e154b569b964d9d028098c05bd9a814d6825abc0
J.R. Mathis <mathiscode@users.noreply.github.com>
2019-04-20 15:49:44 -0500

    Updates

---
+++ precache-manifest.f549f2c01d0a45a5aaea69805c33fa67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c490f7e7d7f4ac3a091",
+    "url": "/whogoverns.me/static/css/main.3f344c7b.chunk.css"
+  },
+  {
+    "revision": "5c490f7e7d7f4ac3a091",
+    "url": "/whogoverns.me/static/js/main.91900e4c.chunk.js"
+  },
+  {
+    "revision": "c47140b733974f4e0f5c",
+    "url": "/whogoverns.me/static/js/runtime~main.eddaa40d.js"
+  },
+  {
+    "revision": "6d665f11cddcd50df2a2",
+    "url": "/whogoverns.me/static/css/2.a09da3aa.chunk.css"
+  },
+  {
+    "revision": "6d665f11cddcd50df2a2",
+    "url": "/whogoverns.me/static/js/2.552c987a.chunk.js"
+  },
+  {
+    "revision": "447bb7f2220f55ea0dea32e577f92eb3",
+    "url": "/whogoverns.me/static/media/default-photo.447bb7f2.jpg"
+  },
+  {
+    "revision": "fdc6ef67fff5f6e0fe153903065a05d9",
+    "url": "/whogoverns.me/index.html"
+  }
+];
