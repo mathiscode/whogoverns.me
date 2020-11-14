6a43fa976f2e18d0f3ef29775d4ed66380d906b8
J.R. Mathis <jr@mathis.network>
2020-11-14 14:18:07 -0600

    Updates

---
+++ precache-manifest.e37428b08fc700efccc6dec7a52cdfe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "346fb7f9ec6f2ccb9e6c",
+    "url": "/static/css/main.65a46b4f.chunk.css"
+  },
+  {
+    "revision": "346fb7f9ec6f2ccb9e6c",
+    "url": "/static/js/main.a8fda9f1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "1cd4582a27d2f63e68b2",
+    "url": "/static/css/2.c45b692d.chunk.css"
+  },
+  {
+    "revision": "1cd4582a27d2f63e68b2",
+    "url": "/static/js/2.8aa318e5.chunk.js"
+  },
+  {
+    "revision": "447bb7f2220f55ea0dea32e577f92eb3",
+    "url": "/static/media/default-photo.447bb7f2.jpg"
+  },
+  {
+    "revision": "1447a1343b79eb883e0115b0d9678b9a",
+    "url": "/index.html"
+  }
+];
