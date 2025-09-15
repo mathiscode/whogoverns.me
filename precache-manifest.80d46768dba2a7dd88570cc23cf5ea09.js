5460c437c56906414e36865c216e1fda3b0505c8
mathiscode <jrmathis.home@gmail.com>
2025-09-15 15:59:16 +0000

    Deploying to gh-pages from  @ 21be4d69efaa122adbaa99854227591a365a7325 🚀

---
+++ precache-manifest.80d46768dba2a7dd88570cc23cf5ea09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c63064c3898fbd4bcd7",
+    "url": "/static/css/main.a29cc156.chunk.css"
+  },
+  {
+    "revision": "5c63064c3898fbd4bcd7",
+    "url": "/static/js/main.3518fc59.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ed3414b097ec5d5000be",
+    "url": "/static/js/2.f7844318.chunk.js"
+  },
+  {
+    "revision": "6c9fe1fafc7be7d75d4effbff0ae6d28",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.41f7579976376a3654260190cbac6a92.js"
+  "/precache-manifest.80d46768dba2a7dd88570cc23cf5ea09.js"
 );
 
 workbox.clientsClaim();
